2171d0ddf3cc1420e4b2845481fd44e4798a7b2a
= <s3949273@student.rmit.edu.au>
2024-05-21 08:08:41 +1000

    reduced usage of termion

--- src/helper.rs
+++ src/helper.rs
@@ -1,21 +1,46 @@
-use std::io::stdin;
+use std::io::{stdin, stdout, Write};
 
 pub fn check_is_input_quit(input:&String)->bool{
     return input == "Quit" || input == "quit" || input == "QUIT"
 }
 
-pub fn confirm_input()->bool{
-    println!("press y to confirm or n to enter again:");
+pub fn take_input(prompt:String, is_pass:bool)->Option<String>{
     let mut buf = "".to_string();
-    stdin().read_line(&mut buf).expect("unable to read line");
-    return buf == "Yes\n" || buf == "yes\n" || buf == "Y\n"|| buf == "y\n";
+    if is_pass{
+        buf = rpassword::prompt_password(prompt).expect("Unable to get password from user");
+        if check_is_input_quit(&buf){
+            return  None;
+        }
+        return Some(buf);
+    }else{
+        print!("{}",prompt.clone()+" ");
+        stdout().flush().expect("unable to flush lines to terminal");
+        stdin().read_line(&mut buf).expect("unable to read line");
+        if check_is_input_quit(&buf){
+            return  None;
+        }
+        return Some(buf[..buf.len()-1].to_string());
+    }
+    
+}
+
+pub fn take_confirmed_input(prompt:String, is_pass:bool)->String{
+    loop{
+        // keep taking input while the user does not confirm
+        let input = take_input(prompt.clone(), is_pass);
+        if input == ""{
+            return input;
+        }
+        let confirmation = take_input("Confirm input? y/n or q to quit".to_string(), false).to_lowercase();
+        if confirmation == "y"{
+            return  input;
+        }if confirmation == "q"{
+            return  "".to_string();
+        }
+    }
 }
 
 pub fn take_password_input()->String{
-    let pass = rpassword::prompt_password("please enter your password:").expect("unable to take pass");
-    if confirm_input(){
-        return pass;
-    }else{
-        return take_password_input();
-    }
+    take_confirmed_input("Please enter your password:".to_string(), true)
 }
+
