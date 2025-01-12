bb59839b517d9042d99e9dee6e83908f30d134ec
AnDrOiDTiTaN1263 <tirthvpatel2003@gmail.com>
2025-01-13 09:31:13 +1100

    initialised nextron frontend

---
+++ frontend/renderer/pages/home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function HomePage() {
+  return (
+    <div className='flex flex-col'>
+      <div id='header' className='flex text-4xl font-bold justify-center'>Password Manager</div>
+
+    </div>
+  )
+}
