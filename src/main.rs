b7089ce9f4e840d74a760171b06fcafcec27710d
AnDrOiDTiTaN1263 <tirthvpatel2003@gmail.com>
2025-01-13 14:42:20 +1100

    added axum boilerplate

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@ mod entry;
 mod password_manager;
 mod helper;
 use password_manager::PasswordManager;
+use axum::{routing::{get, post}, Router};
 
-fn main(){
-    if let Some(mut pm) = PasswordManager::new("PMfiles/safe.pswd".to_string()){
-        pm.menu();
-    }else{
-        println!("could not open a safe file in the directory: PMfiles/safe.pswd");
-    }
+#[tokio::main]
+async fn main(){
+    let app = Router::new().route("/test", get(test));
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000").await.unwrap();
+    axum::serve(listener, app).await.unwrap()
+}
 
+async fn test()->String{
+    return  "Ok -- Connected".to_string();
 }
