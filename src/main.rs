61cc37aff849c6c446a24d6a3cf3456146212ac2
Tirth Patel <tirthvpatel2003@gmail.com>
2025-03-01 14:12:07 +1100

    removed frontend

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,25 @@ mod entry;
 mod password_manager;
 mod helper;
 use password_manager::PasswordManager;
-use axum::{routing::{get, post}, Router};
+use tower_http::{self, cors::CorsLayer, trace::TraceLayer};
+use axum::{http::Response, routing::{get, post}, Json, Router};
 
 #[tokio::main]
 async fn main(){
-    let app = Router::new().route("/test", get(test));
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000").await.unwrap();
+    let app = Router::new()
+        .route("/test", get(test))
+        .layer(CorsLayer::permissive()).layer(TraceLayer::new_for_http())
+        ;
+    let listener: tokio::net::TcpListener = tokio::net::TcpListener::bind("127.0.0.1:8000").await.unwrap();
     axum::serve(listener, app).await.unwrap()
 }
 
-async fn test()->String{
-    return  "Ok -- Connected".to_string();
+async fn test()->Response<String>{
+     Response::new("{\"message\": \"Ok -- connected\", \"status\":200}".to_string())
+    // Response::new("Ok -- connected".to_string())
+
+}   
+
+async fn getVaults(){
+    
 }
